5fc2a9f5e6a8bf42d661d6a25ab8080f6c4d5846
Luis Arcos <1813110043@untels.edu.pe>
2023-01-04 12:02:48 -0500

    EF Parte 02 - Docente - ArcosEs

--- LLAZ_EF/LLAZ_MIAPP/models.py
+++ LLAZ_EF/LLAZ_MIAPP/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Docente(models.Model):
     codigo=models.CharField(max_length=20)
     nombre=models.CharField(max_length=20)
-    fecha_compra=models.DateField()
+    apellido_paterno=models.CharField(max_length=20)
+    apellido_materno=models.CharField(max_length=20)
+    fecha_nacimiento=models.DateField()
     fecha_registro=models.DateField(auto_now_add=True)
+    DNI=models.DateField(max_length=8)
     estado=models.CharField(max_length=20)
